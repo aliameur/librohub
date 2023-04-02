a432290069ab7bf45f0b6a845fa87b5006d0d3e7
Ali <aliameur169@gmail.com>
2023-04-02 18:00:38 +0000

    add flask

--- assets/js/books.js
+++ assets/js/books.js
@@ -7,11 +7,13 @@ async function renderBooks() {
     booksListEl.innerHTML = booksData.results.map((book) =>
         `
     <div class="book__wrapper">
-        <figure class="book__img--wrapper">
-        <div class="book__overlay"></div>
-            <img src="${book.formats["image/jpeg"]}" alt="" class="book__img">
-        </figure>
-        <h3 class="book__title">${book.title}</h3>
+        <div class="book">
+            <div class="book__overlay"></div>
+            <figure class="book__img--wrapper">
+                <img src="${book.formats["image/jpeg"]}" alt="" class="book__img">
+            </figure>
+            <h3 class="book__title">${book.title}</h3>
+        </div>
     </div>
     `).join("");
 
